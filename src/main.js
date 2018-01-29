4aa14114af00a9e969e8da44a2044a364740a5c2
Anatolie Diordita <contact@anatolie.me>
2018-01-29 12:22:40 -0500

    CF-4: Fix missing store for core settings, add back vee-validate library

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from './store'
 
 import VueI18n from 'vue-i18n'
 import VueCookie from 'vue-cookie'
+import VeeValidate from 'vee-validate'
 import VueProgressBar from 'vue-progressbar'
 import Toasted from 'vue-toasted'
 
 Vue.use(VueI18n)
 Vue.use(VueCookie)
+Vue.use(VeeValidate)
 Vue.use(VueProgressBar, {
   color: '#18FF6D',
   failedColor: 'red',
