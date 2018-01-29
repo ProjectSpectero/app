2eb6345c8d6a8653a387429a8cf7987275104e60
Anatolie Diordita <contact@anatolie.me>
2018-01-29 17:17:22 -0500

    CF-11: Add basic TFA form

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,7 +1,9 @@
 // Authentication routes
 
 import Login from '@/components/auth/login'
+import TwoFactorLogin from '@/components/auth/tfa'
 
 export default [
-  { path: '/login', name: 'login', component: Login, meta: { antiAuth: true, layout: 'auth' } }
+  { path: '/login', name: 'login', component: Login, meta: { antiAuth: true, layout: 'auth' } },
+  { path: '/tfa', name: 'tfa', component: TwoFactorLogin, meta: { antiAuth: true, layout: 'auth' } }
 ]
