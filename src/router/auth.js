43e012d6d0e852eb7e392d8458b70ebc1b0ace15
Anatolie Diordita <contact@anatolie.me>
2018-01-31 17:08:54 -0500

    CF-25: Create placeholder component for /verify feature

--- src/router/auth.js
+++ src/router/auth.js
@@ -3,9 +3,11 @@
 import Login from '@/components/auth/login'
 import TwoFactorLogin from '@/components/auth/tfa'
 import Register from '@/components/auth/register'
+import Verify from '@/components/auth/verify'
 
 export default [
   { path: '/login', name: 'login', component: Login, meta: { antiAuth: true, layout: 'auth' } },
   { path: '/tfa', name: 'tfa', component: TwoFactorLogin, meta: { antiAuth: true, layout: 'auth' } },
-  { path: '/register', name: 'register', component: Register, meta: { antiAuth: true, layout: 'auth' } }
+  { path: '/register', name: 'register', component: Register, meta: { antiAuth: true, layout: 'auth' } },
+  { path: '/verify', name: 'verify', component: Verify, meta: { antiAuth: true, layout: 'auth' } }
 ]
