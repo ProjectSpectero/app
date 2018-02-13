571054f2677616c4e51cdafc712a91d68d7e1e37
Sergio Castro <sergio.castro.vale@gmail.com>
2018-02-13 00:18:29 +0000

    CF-38: Integrating stripe workflow

--- src/components/payments/stripe.vue
+++ src/components/payments/stripe.vue
@@ -1,14 +1,21 @@
 <template>
-  <form action="/process-payment" method="post">
+  <form @submit="test" method="post">
     <stripe-checkout
       :stripe-key="stripeKey"
-      :product="product">
+      :product="product"
+      on-success="broadcast">
     </stripe-checkout>
   </form>
 </template>
 
 <script>
 import { StripeCheckout } from 'vue-stripe'
+
+// Workflow:
+// 1) On click, call /order (API)
+// 2) Receive the data for the new order
+// 3) Call /invoice/self
+// 4) Now we can use stripe with the info
 
 export default {
   data () {
@@ -24,6 +31,11 @@ export default {
     stripeKey () {
       console.log(process.env.STRIPE_MODE)
       return (process.env.STRIPE_MODE === 'sandbox') ? process.env.STRIPE_SANDBOX_PUBLIC_KEY : process.env.STRIPE_LIVE_PUBLIC_KEY
+    }
+  },
+  methods: {
+    test () {
+      console.log('here')
     }
   },
   components: {
