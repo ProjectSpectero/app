598aaebe77f29f01a0e63a000555380718ac9104
Sergio Castro <sergio.castro.vale@gmail.com>
2018-02-15 23:02:40 +0000

    CF-40: Yet another paypal test

--- src/router/payments.js
+++ src/router/payments.js
@@ -5,7 +5,7 @@ import paypalCallback from '@/components/payments/paypalCallback'
 import Stripe from '@/components/payments/stripe'
 
 export default [
-  { path: '/payment/process/stripe/:invoiceId', name: 'stripe', component: Stripe },
-  { path: '/payment/process/paypal/:invoiceId', name: 'paypal', component: Paypal },
-  { path: '/payment/paypal/callback', name: 'paypalCallback', component: paypalCallback, meta: { layout: 'auth' } }
+  { path: '/payment/stripe/:invoiceId', name: 'stripe', component: Stripe },
+  { path: '/payment/paypal/callback', name: 'paypalCallback', component: paypalCallback, meta: { layout: 'auth' } },
+  { path: '/payment/paypal/:invoiceId', name: 'paypal', component: Paypal }
 ]
