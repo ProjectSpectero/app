7c5f158a46d1306c60a6bc19605c11ba1bce89f3
Sergio Castro <sergio.castro.vale@gmail.com>
2018-02-22 15:48:58 +0000

    CF-54

--- src/components/layouts/error.vue
+++ src/components/layouts/error.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="error-screen">
+    <sidebar></sidebar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import sidebar from '@/components/common/sidebar'
 
+export default {
+  components: {
+    sidebar
+  }
+}
 </script>
 
 <style lang="scss" scoped>
