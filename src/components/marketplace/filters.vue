cdefbabc69241da7de5b214adbd0f17e4587b79f
Sergio Castro <sergio.castro.vale@gmail.com>
2018-03-23 00:11:42 +0000

    CF-136: Binding filters to search results

--- src/components/marketplace/filters.vue
+++ src/components/marketplace/filters.vue
@@ -4,22 +4,29 @@
       <label for="">Market model</label>
       <div>
         <label>
-          <input type="radio" name="market-model" value="LISTED_SHARED" v-model="nodes.market_model" @change="changeFilter('market_model', '=')">
+          <input type="radio" name="market-model" value="LISTED_SHARED" v-model="nodes.market_model" @change="changeConditionalFilter('market_model', '=')">
           Listed (Shared)
         </label>
       </div>
 
       <div>
         <label>
-          <input type="radio" name="market-model" value="LISTED_DEDICATED" v-model="nodes.market_model" @change="changeFilter('market_model', '=')">
+          <input type="radio" name="market-model" value="LISTED_DEDICATED" v-model="nodes.market_model" @change="changeConditionalFilter('market_model', '=')">
           Listed (Dedicated)
         </label>
       </div>
     </div>
     <div>
       <label>
-        <input type="text" v-model="nodes.asn" @keyup="changeFilter('asn', 'IN')" placeholder="ASN">
+        <input type="number" v-model="nodes.asn" placeholder="ASN">
         ASN
+      </label>
+      <button @click="changeInFilter('asn')">Apply</button>
+    </div>
+    <div>
+      <label :for="'service-type-' + type" v-for="type in serviceTypes" :key="type">
+        <input :id="'service-type-' + type" type="checkbox" @click="toggleServiceType(type)">
+        {{ type }}
       </label>
     </div>
   </form>
@@ -29,6 +36,7 @@
 export default {
   data () {
     return {
+      serviceTypes: ['HTTPProxy', 'OpenVPN'],
       nodes: {
         market_model: null,
         price: null,
@@ -42,16 +50,15 @@ export default {
     }
   },
   methods: {
-    changeFilter (field, operator) {
+    find (field) {
+      return this.rules.findIndex(r => r.field === 'nodes.' + field)
+    },
+    clearFilter (index) {
+      this.rules.splice(index, 1)
+    },
+    updateFilters (filter, index) {
       // Update pre-filled filter with the new value and operator
       // or create a new instance of it
-      let index = this.rules.findIndex(r => r.field === 'nodes.' + field)
-      const filter = {
-        field: 'nodes.' + field,
-        operator: operator,
-        value: (operator !== 'IN') ? this.nodes[field] : [this.nodes[field]]
-      }
-
       if (index !== -1) {
         this.rules[index] = filter
       } else {
@@ -59,6 +66,38 @@ export default {
       }
 
       this.$emit('changedRules', this.rules)
+    },
+    changeConditionalFilter (field, operator) {
+      const index = this.find(field)
+      const filter = {
+        field: 'nodes.' + field,
+        operator: operator,
+        value: this.nodes[field]
+      }
+
+      this.updateFilters(filter, index)
+    },
+    changeInFilter (field) {
+      const index = this.find(field)
+
+      if (!this.nodes[field]) {
+        this.clearFilter(index)
+      } else {
+        const index = this.find(field)
+        const filter = {
+          field: 'nodes.' + field,
+          operator: 'IN',
+          value: [parseInt(this.nodes[field])]
+        }
+
+        this.updateFilters(filter, index)
+      }
+    },
+    toggleServiceType (type) {
+      const obj = this.rules.find(r => r.field === 'nodes.service_type')
+
+      if (obj) {
+      }
     }
   }
 }
