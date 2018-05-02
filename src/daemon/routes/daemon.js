cadb15816f71c68a745a24f1d75fae78e30bee8e
Anatolie Diordita <contact@anatolie.me>
2018-05-02 17:09:39 -0400

    Fix daemon management duplicate routes

--- src/daemon/routes/daemon.js
+++ src/daemon/routes/daemon.js
@@ -1,6 +1,5 @@
 import manage from '@/daemon/components/manage/manage'
 
 export default [
-  { path: '/daemon/:nodeId/:action', name: 'manage', component: manage, meta: { auth: true, layout: 'daemon' } },
-  { path: '/daemon/:nodeId', name: 'manage', component: manage, meta: { auth: true, layout: 'daemon' } }
+  { path: '/daemon/:nodeId/:action?', name: 'manage', component: manage, meta: { auth: true, layout: 'daemon' } }
 ]
