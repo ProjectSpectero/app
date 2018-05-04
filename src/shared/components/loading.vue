f5a28db3ac4da54a7b5764ffdcf2e8a67c80ceb1
Anatolie Diordita <contact@anatolie.me>
2018-05-04 13:25:28 -0400

    CF-142: Marketplace search loading state

--- src/shared/components/loading.vue
+++ src/shared/components/loading.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="alert-msg-centered">
-    <div class="bouncing-loader"><div></div><div></div><div></div></div>
-    <h1>{{ $i18n.t('misc.LOADING') }}</h1>
+    <div class="loader">
+      <div class="bouncing-loader"><div></div><div></div><div></div></div>
+      <h1>{{ $i18n.t('misc.LOADING') }}</h1>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.loader {
+  display: inline-block;
+  padding: 16px 32px 20px 32px;
+  color: $white;
+  background: rgba(0,0,0,0.75);
+  border-radius: 20px;
+}
+</style>
