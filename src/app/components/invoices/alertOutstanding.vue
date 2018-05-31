077b86fb932dfd69fead8368bf6bc181c107ed71
Anatolie Diordita <contact@anatolie.me>
2018-05-31 15:31:32 -0400

    CF-287: Update orders pay now button to display modal instead of invoice

--- src/app/components/invoices/alertOutstanding.vue
+++ src/app/components/invoices/alertOutstanding.vue
@@ -2,10 +2,13 @@
   <div class="outstanding message message-info">
     <h5><span class="icon-dollar-sign"></span> {{ $i18n.t('invoices.BALANCE_DUE') }}</h5>
     <p v-html="$i18n.t('invoices.BALANCE_DUE_TEXT', { amount: currencyString, currency: due.currency })"></p>
+    <pay :invoice="invoice" :due="due" classes="button button-info"></pay>
   </div>
 </template>
 
 <script>
+import pay from './pay'
+
 export default {
   props: {
     invoice: Object,
@@ -15,11 +18,17 @@ export default {
     currencyString () {
       return this.$options.filters.currency(this.due.amount)
     }
+  },
+  components: {
+    pay
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.button {
+  margin-top: 12px;
+}
 @media print {
   .outstanding {
     display: none !important;
