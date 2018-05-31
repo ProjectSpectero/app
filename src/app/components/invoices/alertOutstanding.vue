461812ac292f098eb97be513bbac5b5f74cfcab5
Anatolie Diordita <contact@anatolie.me>
2018-05-31 14:43:36 -0400

    CF-294: Fix currency formatting on due invoice filter, notice added to order view

--- src/app/components/invoices/alertOutstanding.vue
+++ src/app/components/invoices/alertOutstanding.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="outstanding message message-info">
     <h5><span class="icon-dollar-sign"></span> {{ $i18n.t('invoices.BALANCE_DUE') }}</h5>
-    <p v-html="$i18n.t('invoices.BALANCE_DUE_TEXT', { amount: due.amount, currency: due.currency })"></p>
+    <p v-html="$i18n.t('invoices.BALANCE_DUE_TEXT', { amount: currencyString, currency: due.currency })"></p>
   </div>
 </template>
 
@@ -10,6 +10,11 @@ export default {
   props: {
     invoice: Object,
     due: Object
+  },
+  computed: {
+    currencyString () {
+      return this.$options.filters.currency(this.due.amount)
+    }
   }
 }
 </script>
