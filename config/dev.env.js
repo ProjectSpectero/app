65eb0de46dc263ad6e5e23b81ad58c636b89640f
Anatolie Diordita <contact@anatolie.me>
2018-05-31 11:30:31 -0400

    CF-300: Move download links into .env instead of webpack env

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,8 +8,5 @@ module.exports = merge(prodEnv, {
   APP_PORT: '""',
   APP_VERSION: '"v1"',
   APP_COOKIE: '"SPECTERO_APP_AUTH"',
-  DAEMON_COOKIE: '"SPECTERO_DAEMON_AUTH"',
-  DOWNLOAD_LINK_LINUX: '"https://git.io/whatever"',
-  DOWNLOAD_LINK_MACOS: '"https://git.io/whatevermac"',
-  DOWNLOAD_LINK_WINDOWS: '"https://git.io/whateverwin"'
+  DAEMON_COOKIE: '"SPECTERO_DAEMON_AUTH"'
 })
