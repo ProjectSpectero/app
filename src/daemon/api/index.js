b49403b4e349737ba14d44545910a5de0b5be4c2
sergio@spectero.com <Sergio Castro>
2018-07-24 00:13:25 +0100

    CF-501, CF-499, CF-522: Misc fixes to API error handling.

--- src/daemon/api/index.js
+++ src/daemon/api/index.js
@@ -4,14 +4,17 @@ import store from '@/store'
 
 async function API (method, path, data, success, failed) {
   // Setting up the DAEMON project
-  const cookieName = process.env['DAEMON_COOKIE']
+  const cookieName = process.env.DAEMON_COOKIE
+  const cookie = getCookie(cookieName)
+
   let project = {
     cookieName: cookieName,
-    cookie: (getCookie(cookieName) !== null) ? JSON.parse(getCookie(cookieName)) : null,
+    cookie: (cookie !== null) ? JSON.parse(cookie) : null,
     protocol: store.getters['daemonAuth/protocol'] ? store.getters['daemonAuth/protocol'] + '://' : location.protocol + '//',
     port: store.getters['daemonAuth/port'] ? ':' + store.getters['daemonAuth/port'] : '',
     endpoint: store.getters['daemonAuth/ip'] ? store.getters['daemonAuth/ip'] : location.hostname,
-    version: store.getters['daemonAuth/version']
+    version: store.getters['daemonAuth/version'],
+    timeout: 5000
   }
 
   await requestAPI(project, method, path, data, success, failed)
