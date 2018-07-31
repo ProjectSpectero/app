70243a99a6803395949b2a72f74885d11eda8a4d
Anatolie Diordita <contact@anatolie.me>
2018-07-31 00:38:48 -0400

    CF-532: Remove old payment gateways in preparation for new checkout route

--- src/app/routes/payments.js
+++ src/app/routes/payments.js
@@ -1,17 +1,8 @@
-// Authentication routes
-
-import Paypal from '@/app/components/payments/paypal'
+// Payment routes
 import paypalCallback from '@/app/components/payments/paypalCallback'
-import Stripe from '@/app/components/payments/stripe'
-import creditPayment from '@/app/components/payments/accountCredit'
 import addCredit from '@/app/components/payments/addCredit'
-import Crypto from '@/app/components/payments/crypto'
 
 export default [
-  { path: '/payment/stripe/:invoiceId', name: 'stripe', component: Stripe, meta: { auth: true } },
   { path: '/payment/paypal/callback', name: 'paypalCallback', component: paypalCallback, meta: { auth: true } },
-  { path: '/payment/paypal/:invoiceId', name: 'paypal', component: Paypal, meta: { auth: true } },
-  { path: '/payment/credit/paypal/:invoiceId', name: 'paypalCredit', component: creditPayment, meta: { auth: true } },
-  { path: '/payment/credit/add', name: 'addCredit', component: addCredit, meta: { auth: true } },
-  { path: '/payment/crypto/:invoiceId', name: 'crypto', component: Crypto, meta: { auth: true } }
+  { path: '/payment/credit/add', name: 'addCredit', component: addCredit, meta: { auth: true } }
 ]
