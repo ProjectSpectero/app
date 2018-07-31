78291c80173130ed4fae214e9faade78ba3ebc50
Anatolie Diordita <contact@anatolie.me>
2018-07-31 00:47:00 -0400

    CF-532: Adjust credit card component for new checkout gateway handler

--- src/app/components/payments/creditCard.vue
+++ src/app/components/payments/creditCard.vue
@@ -1,16 +1,73 @@
 <template>
-  <div v-if="!error">
-    <div v-if="!loading">
-      {{ $i18n.t('payments.PAYMENT_SUCCESS') }}
-    </div>
-    <div v-else>
-      <card
-        :stripe="stripeKey"
-        :options="stripeOptions"
-        class="stripe-card"
-        @change="setProcessedStatus($event.complete)"
-      />
-      <div class="label label-save-cc">
+  <div>
+    <div
+      v-if="!paid"
+      class="cc-form">
+      <div
+        v-if="formError"
+        class="message message-error">{{ formError }}</div>
+
+      <div class="form-input col-container reduce-spacing billing-fields">
+        <div class="col">
+          <float-label>
+            <input
+              v-validate="rules.name"
+              v-model="customerData.customer_name"
+              :disabled="loading || disableBillingFields"
+              :class="{'input-error': errors.has('name')}"
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              class="input"
+              data-vv-as="name">
+          </float-label>
+
+          <span
+            v-show="errors.has('name')"
+            class="input-error-message">
+            {{ errors.first('name') }}
+          </span>
+        </div>
+        <div class="col">
+          <float-label>
+            <input
+              v-validate="rules.zip"
+              v-model="customerData.post_code"
+              :disabled="loading || disableBillingFields"
+              :class="{'input-error': errors.has('zip')}"
+              type="text"
+              name="zip"
+              placeholder="Zip/Postal Code"
+              class="input"
+              data-vv-as="zip">
+          </float-label>
+
+          <span
+            v-show="errors.has('zip')"
+            class="input-error-message">
+            {{ errors.first('zip') }}
+          </span>
+        </div>
+        <div v-if="disableBillingFields">
+          <router-link
+            :to="{ name: 'settings', params: { tab: 'payment' }, query: { continueCcPayment: invoice.id } }"
+            class="button">
+            Edit
+          </router-link>
+        </div>
+      </div>
+
+      <div class="card-container">
+        <card
+          :stripe="stripeKey"
+          :options="stripeCardOptions"
+          class="stripe-card"
+        />
+        <div
+          v-if="loading"
+          class="card-overlay"/>
+      </div>
+      <div class="label mt-3">
         <p-input
           id="cc-save"
           v-model="saveCard"
@@ -21,16 +78,26 @@
         </p-input>
       </div>
 
+      <p class="order-total">
+        Order Total: <span class="amount">{{ due.amount | currency }} {{ due.currency }}</span>
+      </p>
+
       <button
-        v-if="!paid"
-        :disabled="!processed"
-        class="button-md button-success button-full button-pay"
+        :disabled="loading"
+        :class="{'button-loading': loading}"
+        class="button-lg button-success button-pay"
         @click.stop="pay">
-        {{ $i18n.t('payments.BUTTON_PROCESS_PAYMENT') }}
+        {{ $i18n.t('payments.BUTTON_PAY_NOW') }}
       </button>
-      <div
-        v-else
-        class="mt-3">{{ $i18n.t('payments.PAYMENT_PROCESSING') }}</div>
+
+      <p class="order-secure">
+        <span class="icon-lock"/> {{ $i18n.t('payments.PAY_SECURE') }}
+        <span
+          v-tooltip="$i18n.t('payments.PAY_SECURE_DETAILS')"
+          class="tooltip-text-trigger">
+          {{ $i18n.t('misc.LEARN_MORE') }} &raquo;
+        </span>
+      </p>
     </div>
   </div>
 </template>
@@ -39,36 +106,46 @@
 import { mapGetters } from 'vuex'
 import { Card, createToken } from 'vue-stripe-elements-plus'
 import paymentAPI from '@/app/api/payment'
-import invoiceAPI from '@/app/api/invoice'
 
 export default {
   components: {
     Card
   },
   props: {
-    invoiceId: {
-      type: Number,
+    invoice: {
+      type: Object,
+      required: true
+    },
+    due: {
+      type: Object,
+      required: true
+    },
+    customer: {
+      type: Object,
       required: true
     }
   },
   data () {
     return {
-      status: '',
-      error: null,
-      invoice: null,
+      loading: false,
+      formError: null,
       paid: false,
-      processed: false,
-      paymentDetails: [
-        'name',
-        'address_line_1',
-        'city',
-        'state',
-        'post_code',
-        'country'
-      ],
+      customerData: {},
+      customerComplete: false,
+      disableBillingFields: false,
       saveCard: true,
-      stripeOptions: {
-        // https://stripe.com/docs/stripe.js#element-options
+      stripeCardOptions: { // https://stripe.com/docs/stripe.js#element-options
+        hidePostalCode: true
+      },
+      rules: {
+        name: {
+          required: true,
+          max: 50
+        },
+        zip: {
+          required: true,
+          max: 10
+        }
       }
     }
   },
@@ -81,100 +158,92 @@ export default {
     }
   },
   async created () {
-    await this.fetchInvoice()
-    console.log(this.invoice)
-    // Is this a valid and payable invoice?
-    // Then we can proceed and validate if user details
-    // are correctly set (we need to send them to Stripe)
-    if (this.invoice && this.invoice.status === 'UNPAID') {
-      this.validateUserDetails()
-    } else {
-      this.fetchFailed()
-    }
+    this.customerData = Object.assign({}, this.customer.data)
+    this.customerComplete = this.customer.complete
+    this.disableBillingFields = (this.customerData.customer_name && this.customerData.post_code)
   },
   methods: {
-    validateUserDetails () {
-      let error = false
-
-      this.paymentDetails.forEach(field => {
-        console.log(this.user[field])
-        if (this.user[field] === null || this.user[field] === '') {
-          error = true
-        }
-      })
-
-      if (error) {
-        this.redirectToProfile()
-      }
-    },
     pay () {
-      const details = {
-        name: this.user.name,
-        address_line1: this.user.address_line_1,
-        address_line2: this.user.address_line_2 || '',
-        address_city: this.user.city,
-        address_state: this.user.state,
-        address_zip: this.user.post_code,
-        address_country: this.user.country
-      }
+      this.$validator.validateAll().then(result => {
+        if (!result) {
+          this.formError = this.$i18n.t(`errors.VALIDATION_FAILED`)
+        } else {
+          this.loading = true
 
-      // Stripe token issued
-      createToken(details).then(stripeData => {
-        this.paid = true
+          // Format customer data for Stripe's format
+          const stripeCustomer = {
+            name: this.customerData.customer_name,
+            address_line1: this.customerData.address_line_1,
+            address_line2: this.customerData.address_line_2 || '',
+            address_city: this.customerData.city,
+            address_state: this.customerData.state,
+            address_zip: this.customerData.post_code,
+            address_country: this.customerData.country
+          }
 
-        // Process stripe payment on our API
-        this.processStripe(stripeData)
-      }).catch((e) => {
-        console.log(e)
-        this.redirectToProfile()
-      })
-    },
-    redirectToProfile () {
-      this.$toasted.error(this.$i18n.t('errors.INSUFFICIENT_PAYMENT_DETAILS'))
-      this.$router.push({ name: 'settings', params: { tab: 'payment', fromInvoice: this.invoiceId } })
-    },
-    async fetchInvoice () {
-      await invoiceAPI.invoice({
-        data: { id: this.invoiceId },
-        success: response => {
-          this.invoice = response.data.result
-        },
-        fail: (e) => {
-          console.log(e)
+          // Issue Stripe customer token
+          createToken(stripeCustomer).then(stripeResponse => {
+            if (stripeResponse.error) {
+              this.formError = this.$i18n.t('payments.STRIPE_ERROR', { msg: stripeResponse.error.message })
+              this.loading = false
+            } else {
+              this.formError = null
+
+              // Process stripe payment on our API
+              this.processStripe(stripeResponse)
+            }
+          }).catch((e) => {
+            this.formError = this.$i18n.t('misc.UNKNOWN_ERROR')
+            this.loading = false
+            console.error('Error while creating Stripe token', e)
+          })
         }
       })
-    },
-    fetchFailed () {
-      this.$toasted.error(this.$i18n.t('payments.INVOICE_STATUS_MISMATCH'))
-      this.$router.push({ name: 'invoices' })
     },
     processStripe (stripeData) {
       paymentAPI.processStripe({
         data: {
-          invoiceId: this.invoiceId,
+          invoiceId: this.invoice.id,
           stripeToken: stripeData.token.id,
           save: this.saveCard
         },
         success: async processResponse => {
-          this.$toasted.success(this.$i18n.t('payments.PAYMENT_ACCEPTED'), { duration: 10000 })
-          this.$router.push({ name: 'invoice', params: { id: this.invoiceId } })
+          this.paid = true
+          this.$emit('paymentComplete')
         },
         fail: error => {
-          this.$toasted.error(this.errorAPI(error, 'payments'))
-          this.$router.push({ name: 'invoices' })
+          this.loading = false
+          this.formError = this.errorAPI(error, 'payments')
+          console.error('Stripe process error', error)
         }
       })
-    },
-    setProcessedStatus (status) {
-      this.processed = status
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.label-save-cc {
-  border-radius: 4px;
-  margin: 16px 0;
+.card-container {
+  position: relative;
+
+  .card-overlay {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(255,255,255,0.6);
+  }
+}
+.order-secure {
+  margin-top: $pad;
+  opacity: 0.5;
+}
+.billing-fields {
+  align-items: center;
 }
 </style>
