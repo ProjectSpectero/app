e1f4de34fa0cfb73d5ea27a4208ada65f2cc2f9e
sergio@spectero.com <Sergio Castro>
2018-08-14 00:03:54 +0100

    Updated staging config

--- config/staging.env.js
+++ config/staging.env.js
@@ -1,5 +1,4 @@
 const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
 const config = require('dotenv').config()
 const dotenvVars = {}
 
@@ -9,11 +8,8 @@ if (config.parsed !== undefined) {
   }
 }
 
-module.exports = merge(prodEnv, dotenvVars, {
-  NODE_ENV: '"staging"',
-  APP_ENDPOINT: '"dev.spectero.com"',
-  APP_HTTPS: true,
-  APP_PORT: '""',
+module.exports = merge(dotenvVars, {
+  NODE_ENV: '"production"',
   APP_VERSION: '"v1"',
   APP_COOKIE: '"SPECTERO_APP_AUTH"',
   DAEMON_COOKIE: '"SPECTERO_DAEMON_AUTH"',
