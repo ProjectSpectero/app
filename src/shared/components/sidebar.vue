aab7581233e6036ee39d959af9ce8a17b63718ca
Anatolie Diordita <contact@anatolie.me>
2018-08-20 09:39:05 -0400

    CF-648: Responsive sidebar fix

--- src/shared/components/sidebar.vue
+++ src/shared/components/sidebar.vue
@@ -8,179 +8,195 @@
           class="logo logo-sm"/>
       </router-link>
     </div>
-    <div class="menu-items middle">
-      <template v-if="user">
-        <!-- <section class="nav-section">
-          <ul>
-            <li>
-              <router-link :to="{ name: 'dashboard' }">
-                <span class="icon-pie-chart"></span>
-                {{ $i18n.t('misc.DASHBOARD') }}
-              </router-link>
-            </li>
-          </ul>
-        </section> -->
-        <section
-          v-if="isAdmin"
-          class="nav-section">
-          <h5>{{ $i18n.t('misc.ADMIN') }}</h5>
-          <ul>
-            <li>
-              <router-link :to="{ name: 'users-list' }">
-                <span class="icon-pie-chart"/>
-                {{ $i18n.t('misc.USERS') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section class="nav-section">
-          <ul>
-            <li>
-              <router-link :to="{ name: 'pro' }">
-                <span class="icon-zap"/>
-                {{ $i18n.t('misc.PRO') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section
-          v-if="isEnterprise"
-          class="nav-section">
-          <h5>{{ $i18n.t('misc.ENTERPRISE') }}</h5>
-          <ul>
-            <li>
-              <router-link :to="{ name: 'enterpriseOrders' }">
-                <span class="icon-server"/>
-                {{ $i18n.t('misc.ENTERPRISE') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section class="nav-section">
-          <h5>{{ $i18n.t('misc.DAEMON') }}</h5>
-          <ul>
-            <li>
-              <router-link
-                :to="{ name: 'nodes' }"
-                :class="{'router-link-active': ['node', 'nodesGroupCreate', 'nodeGroupEdit', 'daemon', 'daemon-service-HTTPProxy', 'daemon-service-OpenVPN'].includes($route.name)}">
-                <span class="icon-hard-drive"/>
-                {{ $i18n.t('misc.NODES') }}
-              </router-link>
-            </li>
-            <li>
-              <router-link :to="{ name: 'downloads' }">
-                <span class="icon-download-cloud"/>
-                {{ $i18n.t('misc.DOWNLOADS') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section class="nav-section">
-          <h5>{{ $i18n.t('misc.MARKET') }}</h5>
-          <ul>
-            <li>
-              <router-link :to="{ name: 'market' }">
-                <span class="icon-shopping-bag"/>
-                {{ $i18n.t('misc.MARKET') }}
-              </router-link>
-            </li>
-            <li>
-              <router-link :to="{ name: 'cart' }">
-                <span class="icon-shopping-cart"/>
-                {{ $i18n.t('misc.CART') }}
-                <span
-                  v-if="count"
-                  class="count">{{ count }}</span>
-              </router-link>
-            </li>
-            <li>
-              <router-link
-                :to="{ name: 'orders' }"
-                :class="{'router-link-active': ['order', 'orderResources', 'orderInvoices'].includes($route.name)}">
-                <span class="icon-briefcase"/>
-                {{ $i18n.t('misc.ORDERS') }}
-              </router-link>
-            </li>
-            <li>
-              <router-link
-                :to="{ name: 'invoices' }"
-                :class="{'router-link-active': ['invoice', 'checkout'].includes($route.name)}">
-                <span class="icon-dollar-sign"/>
-                {{ $i18n.t('misc.INVOICES') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section class="nav-section">
-          <h5>{{ $i18n.t('misc.HELP') }}</h5>
-          <ul>
-            <li>
-              <router-link :to="{ name: 'docs' }">
-                <span class="icon-help-circle"/>
-                {{ $i18n.t('misc.DOCUMENTATION') }}
-              </router-link>
-            </li>
-            <li v-if="supportLink">
-              <a
-                :href="supportLink"
-                target="_blank">
-                <span class="icon-life-buoy"/>
-                {{ $i18n.t('misc.SUPPORT') }}
-              </a>
-            </li>
-          </ul>
-        </section>
-      </template>
-      <template v-else>
-        <section class="nav-section">
-          <ul>
-            <li>
-              <a href="https://spectero.com">
-                <span class="icon-home"/>
-                {{ $i18n.t('misc.HOME') }}
-              </a>
-            </li>
-            <li>
-              <router-link :to="{ name: 'pro' }">
-                <span class="icon-zap"/>
-                {{ $i18n.t('misc.PRO') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section class="nav-section">
-          <h5>{{ $i18n.t('misc.DAEMON') }}</h5>
-          <ul>
-            <li>
-              <router-link :to="{ name: 'downloads' }">
-                <span class="icon-download-cloud"/>
-                {{ $i18n.t('misc.DOWNLOADS') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-        <section class="nav-section">
-          <h5>{{ $i18n.t('misc.ACCOUNT') }}</h5>
-          <ul>
-            <li>
-              <router-link
-                :to="{ name: 'login', query: { redirect: '/' } }"
-                :class="{'router-link-active': ['login'].includes($route.name)}">
-                <span class="icon-log-in"/>
-                {{ $i18n.t('misc.LOGIN') }}
-              </router-link>
-            </li>
-            <li>
-              <router-link :to="{ name: 'register' }">
-                <span class="icon-user-plus"/>
-                {{ $i18n.t('misc.REGISTER') }}
-              </router-link>
-            </li>
-          </ul>
-        </section>
-      </template>
+
+    <a
+      :class="{ 'active': !menuCollapsed }"
+      class="hamburger"
+      @click="toggleMenu()">
+      <span :class="{ 'icon-menu': menuCollapsed, 'icon-x': !menuCollapsed }" />
+    </a>
+
+    <div
+      :class="{ 'show': !menuCollapsed }"
+      class="responsive-menu-overlay" />
+
+    <div
+      :class="{ collapsed: menuCollapsed }"
+      class="sidebar-menu">
+      <div class="menu-items middle">
+        <template v-if="user">
+          <!-- <section class="nav-section">
+            <ul>
+              <li>
+                <router-link :to="{ name: 'dashboard' }">
+                  <span class="icon-pie-chart"></span>
+                  {{ $i18n.t('misc.DASHBOARD') }}
+                </router-link>
+              </li>
+            </ul>
+          </section> -->
+          <section
+            v-if="isAdmin"
+            class="nav-section">
+            <h5>{{ $i18n.t('misc.ADMIN') }}</h5>
+            <ul>
+              <li>
+                <router-link :to="{ name: 'users-list' }">
+                  <span class="icon-pie-chart"/>
+                  {{ $i18n.t('misc.USERS') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section class="nav-section">
+            <ul>
+              <li>
+                <router-link :to="{ name: 'pro' }">
+                  <span class="icon-zap"/>
+                  {{ $i18n.t('misc.PRO') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section
+            v-if="isEnterprise"
+            class="nav-section">
+            <h5>{{ $i18n.t('misc.ENTERPRISE') }}</h5>
+            <ul>
+              <li>
+                <router-link :to="{ name: 'enterpriseOrders' }">
+                  <span class="icon-server"/>
+                  {{ $i18n.t('misc.ENTERPRISE') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section class="nav-section">
+            <h5>{{ $i18n.t('misc.DAEMON') }}</h5>
+            <ul>
+              <li>
+                <router-link
+                  :to="{ name: 'nodes' }"
+                  :class="{'router-link-active': ['node', 'nodesGroupCreate', 'nodeGroupEdit', 'daemon', 'daemon-service-HTTPProxy', 'daemon-service-OpenVPN'].includes($route.name)}">
+                  <span class="icon-hard-drive"/>
+                  {{ $i18n.t('misc.NODES') }}
+                </router-link>
+              </li>
+              <li>
+                <router-link :to="{ name: 'downloads' }">
+                  <span class="icon-download-cloud"/>
+                  {{ $i18n.t('misc.DOWNLOADS') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section class="nav-section">
+            <h5>{{ $i18n.t('misc.MARKET') }}</h5>
+            <ul>
+              <li>
+                <router-link :to="{ name: 'market' }">
+                  <span class="icon-shopping-bag"/>
+                  {{ $i18n.t('misc.MARKET') }}
+                </router-link>
+              </li>
+              <li>
+                <router-link :to="{ name: 'cart' }">
+                  <span class="icon-shopping-cart"/>
+                  {{ $i18n.t('misc.CART') }}
+                  <span
+                    v-if="count"
+                    class="count">{{ count }}</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'orders' }"
+                  :class="{'router-link-active': ['order', 'orderResources', 'orderInvoices'].includes($route.name)}">
+                  <span class="icon-briefcase"/>
+                  {{ $i18n.t('misc.ORDERS') }}
+                </router-link>
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'invoices' }"
+                  :class="{'router-link-active': ['invoice', 'checkout'].includes($route.name)}">
+                  <span class="icon-dollar-sign"/>
+                  {{ $i18n.t('misc.INVOICES') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section class="nav-section">
+            <h5>{{ $i18n.t('misc.HELP') }}</h5>
+            <ul>
+              <li>
+                <router-link :to="{ name: 'docs' }">
+                  <span class="icon-help-circle"/>
+                  {{ $i18n.t('misc.DOCUMENTATION') }}
+                </router-link>
+              </li>
+              <li v-if="supportLink">
+                <a
+                  :href="supportLink"
+                  target="_blank">
+                  <span class="icon-life-buoy"/>
+                  {{ $i18n.t('misc.SUPPORT') }}
+                </a>
+              </li>
+            </ul>
+          </section>
+        </template>
+        <template v-else>
+          <section class="nav-section">
+            <ul>
+              <li>
+                <a href="https://spectero.com">
+                  <span class="icon-home"/>
+                  {{ $i18n.t('misc.HOME') }}
+                </a>
+              </li>
+              <li>
+                <router-link :to="{ name: 'pro' }">
+                  <span class="icon-zap"/>
+                  {{ $i18n.t('misc.PRO') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section class="nav-section">
+            <h5>{{ $i18n.t('misc.DAEMON') }}</h5>
+            <ul>
+              <li>
+                <router-link :to="{ name: 'downloads' }">
+                  <span class="icon-download-cloud"/>
+                  {{ $i18n.t('misc.DOWNLOADS') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+          <section class="nav-section">
+            <h5>{{ $i18n.t('misc.ACCOUNT') }}</h5>
+            <ul>
+              <li>
+                <router-link
+                  :to="{ name: 'login', query: { redirect: '/' } }"
+                  :class="{'router-link-active': ['login'].includes($route.name)}">
+                  <span class="icon-log-in"/>
+                  {{ $i18n.t('misc.LOGIN') }}
+                </router-link>
+              </li>
+              <li>
+                <router-link :to="{ name: 'register' }">
+                  <span class="icon-user-plus"/>
+                  {{ $i18n.t('misc.REGISTER') }}
+                </router-link>
+              </li>
+            </ul>
+          </section>
+        </template>
+      </div>
+      <sidebar-dropdown v-if="user"/>
     </div>
-    <sidebar-dropdown v-if="user"/>
   </div>
 </template>
 
@@ -191,6 +207,11 @@ import sidebarDropdown from './sidebarDropdown'
 export default {
   components: {
     sidebarDropdown
+  },
+  data () {
+    return {
+      menuCollapsed: true
+    }
   },
   computed: {
     ...mapGetters({
@@ -221,7 +242,10 @@ export default {
       fetchPlans: 'market/fetchPlans',
       refreshCart: 'cart/refresh',
       fetchSupportLink: 'settings/fetchSupportLink'
-    })
+    }),
+    toggleMenu () {
+      this.menuCollapsed = !this.menuCollapsed
+    }
   }
 }
 </script>
