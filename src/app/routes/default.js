02a97a468ad1b86dc59a0cd53937d2a2c0d6fe2f
Anatolie Diordita <contact@anatolie.me>
2018-09-19 16:21:25 -0400

    Remove redundant UI helper component

--- src/app/routes/default.js
+++ src/app/routes/default.js
@@ -1,9 +1,7 @@
 // Default routes
 
 import downloads from '@/app/views/misc/downloads'
-import ui from '@/app/views/ui'
 
 export default [
-  { path: '/downloads', name: 'downloads', component: downloads, meta: { auth: false } },
-  { path: '/ui', name: 'ui', component: ui, meta: { auth: true } }
+  { path: '/downloads', name: 'downloads', component: downloads, meta: { auth: false } }
 ]
